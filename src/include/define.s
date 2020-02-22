737e91a12feaf231236aa563005a324bc766ede0
Horita Noboru <61climb@gmail.com>
2020-02-22 22:14:24 +0900

    implement zero division interrupt

--- src/include/define.s
+++ src/include/define.s
@@ -10,3 +10,5 @@
     KERNEL_LOAD     equ     0x0010_1000
     KERNEL_SIZE     equ     (1024 * 8)      ; カーネルサイズ
     KERNEL_SECT     equ     (KERNEL_SIZE / SECT_SIZE)
+
+    VECT_BASE       equ     0x0010_0000     ; 0010_0000:0010_07FF
