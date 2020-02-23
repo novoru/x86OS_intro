682f1b88640245e57998a95d107bfc8741a14bc1
Horita Noboru <61climb@gmail.com>
2020-02-23 10:42:22 +0900

    implement multitasling

--- src/include/define.s
+++ src/include/define.s
@@ -12,3 +12,9 @@
     KERNEL_SECT     equ     (KERNEL_SIZE / SECT_SIZE)
 
     VECT_BASE       equ     0x0010_0000     ; 0010_0000:0010_07FF
+
+    STACK_BASE      equ     0x0010_3000
+    STACK_SIZE      equ     1024
+
+    SP_TASK_0       equ     STACK_BASE + (STACK_SIZE * 1)
+    SP_TASK_1       equ     STACK_BASE + (STACK_SIZE * 2)
--- src/include/macro.s
+++ src/include/macro.s
@@ -15,24 +15,45 @@
 %endmacro
 
 %macro  set_vect 1-*
-    push    eax
-    push    edi
+        push    eax
+        push    edi
 
-    mov     edi, VECT_BASE + (%1 * 8)
-    mov     eax, %2
+        mov     edi, VECT_BASE + (%1 * 8)
+        mov     eax, %2
 
-    mov     [edi + 0], ax
-    shr     eax, 16
-    mov     [edi + 6], ax
+        mov     [edi + 0], ax
+        shr     eax, 16
+        mov     [edi + 6], ax
 
-    pop     edi
-    pop     eax
+        pop     edi
+        pop     eax
 %endmacro
 
 %macro  outp 2
-    mov     al, %2
-    out     %1, al
+        mov     al, %2
+        out     %1, al
 %endmacro
+
+%macro  set_desc 2-*
+        push    eax
+        push    edi
+
+        mov     edi, %1
+        mov     eax, %2
+    %if 3 == %0
+        mov     [edi + 0], %3
+    %endif
+
+        mov     [edi + 2], ax
+        shr     eax, 16
+        mov     [edi + 4], al
+        mov     [edi + 7], ah
+
+        pop     edi
+        pop     eax
+%endmacro
+
+
 
 struc drive
         .no     resw    1           ; ドライブ番号
